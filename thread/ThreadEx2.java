0abb2a12e1e244ca043efa2094a51c1b6daf8958
seongsee <seongsee@gmail.com>
2019-12-04 22:30:41 +0900

    - 컬렉션 프레임워크 HashMap 예제 학습  - Java 환경에서 스레드 프로그래밍 예제 학습

---
+++ thread/ThreadEx2.java
@@ -0,0 +1,20 @@
+class ThreadEx2 {
+	public static void main(String[] args) throws Exception {
+		MyThreadEx2_1 t1 = new MyThreadEx2_1();
+		t1.start();
+	}
+}
+
+class MyThreadEx2_1 extends Thread {
+	public void run() {
+		throwException();
+	}
+
+	public void throwException() {
+		try {
+			throw new Exception();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
